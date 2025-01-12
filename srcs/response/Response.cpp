dfb5aa2e58293fb12845cca2f4c9659217e89fe2
Mohammed Al Morabit <mal-mora@e2r9p15.1337.ma>
2025-01-12 11:34:54 +0100

    need yo handel multiple response

--- srcs/response/Response.cpp
+++ srcs/response/Response.cpp
@@ -2,44 +2,23 @@
 
 Response::Response()
 {
+    size = 0;
+    file.clear();
+    firstCall = 1;
 }
-// std::string Response::processGetResponse(RequestParse &request)
-// {
-//     std::string data = FileToString(request);
-//     this->contentType = "image/jpeg";
-//     return data;
-// }
-// std::string Response::FileToString(std::string const &fileName)
-// {
-//     std::ifstream file(fileName, std::ios::binary);
-//     std::ostringstream ostr;
-//     std::string format;
 
-//     if (!file.is_open())
-//         Utility::ShowErrorExit("Error in Open File"); //should response error 404
-//     ostr << file.rdbuf();
-//     size_t pos = fileName.find(".");
-//     if (pos != std::string::npos)
-//         format = fileName.substr(pos + 1);
-//     else
-//         format = "";
-//     if(format == "json")
-//         this->contentType = "application/json";
-//     else if(format == "html")
-//         this->contentType = "text/html";
-//     else
-//         this->contentType = "text/plain";
-//     return ostr.str();
-// }
-
-std::string Response::FileToString(std::ifstream &file, RequestParse &request)
+Response::~Response()
 {
+    
+}
 
+std::string Response::FileToString(RequestParse &request)
+{
     std::ostringstream ostr;
     std::string format;
-    std::string buf(1028, '\0');
+    std::string buf(1024, '\0');
 
-    file.read(&buf[0], 1028);
+    file.read(&buf[0], 1024);
     size_t pos = request._url.find(".");
     if (pos != std::string::npos)
         format = request._url.substr(pos + 1);
@@ -59,18 +38,16 @@ std::string Response::FileToString(std::ifstream &file, RequestParse &request)
     return buf;
 }
 
-int getFileSize(std::ifstream &file)
+int Response::getFileSize()
 {
-    int size;
-
     file.seekg(0, std::ios::end);
     size = file.tellg();
     file.seekg(0, std::ios::beg);
     return size;
 }
-// 1572009
 
-std::string Response::getHeader(RequestParse &request, int size, const std::string &statusLine)
+
+std::string Response::getHeader(RequestParse &request, const std::string &statusLine)
 {
     std::ostringstream bodySize;
     std::ostringstream response;
@@ -91,12 +68,10 @@ std::string Response::getHeader(RequestParse &request, int size, const std::stri
     response << "\r\n";
     return response.str();
 }
+
 std::string Response::processResponse(RequestParse &request, int isSuccess)
 {
     std::string statusLine;
-    static int firstCall = 1;
-    static int size = 0;
-    static std::ifstream file;
     std::string filename;
 
     statusLine = "HTTP/1.1 200 OK\r\n";
@@ -105,25 +80,25 @@ std::string Response::processResponse(RequestParse &request, int isSuccess)
         filename = "error.html";
         statusLine = "HTTP/1.1 400 Bad Request\r\n";
     }
-    else if(firstCall && request.method() == ePOST)
+    else if (firstCall && request.method() == ePOST)
         file.open("post.json");
     else
         filename = request._url;
     if (firstCall)
     {
-        if(request.method() != ePOST)
+        if (request.method() != ePOST)
             file.open(filename, std::ios::binary);
         if (!file.is_open())
         {
             statusLine = "HTTP/1.1 400 Bad Request\r\n";
             file.open("error.html");
         }
-        size = getFileSize(file);
+        size = getFileSize();
     }
     else
-        return FileToString(file, request);
+        return FileToString(request);
     firstCall = 0;
-    return getHeader(request, size, statusLine);
+    return getHeader(request, statusLine);
 }
 
 std::string Response::getResponse(RequestParse &request)
@@ -135,4 +110,11 @@ std::string Response::getResponse(RequestParse &request)
     else
         str = processResponse(request, 1);
     return str;
+}
+
+void Response::ClearVars()
+{
+    size = 0;
+    file.clear();
+    firstCall = 1;
 }
