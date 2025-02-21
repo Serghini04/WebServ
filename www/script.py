f86d71cac2d7b47a5c5855baa744da34c1e061c7
simorabit <simorabit04@gmail.com>
2025-02-21 15:32:56 +0100

    fix segv problem in server

--- www/script.py
+++ www/script.py
@@ -6,9 +6,10 @@ import sys
 sys.stdout.flush()
 
 # Print required CGI headers
-print("HTTP/1.1 200 OK") # Separate headers from body
-print("Content-Length: 115") # Separate headers from body
-print("Content-Type: text/html\r\n\r\n") # Separate headers from body
+# print("HTTP/1.1 200 OK") # Separate headers from body
+print("HTTP/1.1 200 OK\r\n", end="")
+print("Content-Length: 115\r\n", end="") # Separate headers from body
+print("Content-Type: text/html\r\n\r\n", end="") # Separate headers from body
 
 # HTML response body
 print("<html>")
